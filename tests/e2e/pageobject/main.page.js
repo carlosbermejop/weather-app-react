7f1617e43eabcdfc8788f0308340aa2407a07bc9
Carlos Bermejo Pérez <carlosbermejop@gmail.com>
2021-03-13 18:43:15 +0100

    [REA] Tests updated

--- tests/e2e/pageobject/main.page.js
+++ tests/e2e/pageobject/main.page.js
@@ -3,6 +3,10 @@ const Page = require("./Page");
 class MainPage extends Page {
     
     get textHelloWorldElement () { return $("#App") }
+
+    open() {
+        super.open("weather-app-react/");
+    }
 }
 
 module.exports = new MainPage();
